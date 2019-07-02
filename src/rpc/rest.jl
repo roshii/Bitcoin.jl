cc23e2d86e7a70ccb836e9be739c2fc1dd8cda09
roshii <roshii@riseup.net>
2019-07-02 09:47:10 +0200

    Simplify `get_tx`

--- src/rpc/rest.jl
+++ src/rpc/rest.jl
@@ -27,7 +27,7 @@ function get_tx(key::String; url::String="", testnet::Bool=false)
     if tx.segwit
         computed = id(tx)
     else
-        computed = bytes2hex(reverse!(copy(hash256(raw))))
+        computed = bytes2hex(reverse(hash256(raw)))
     end
     if id(tx) != key
         error("not the same id : ", id(tx),
@@ -63,7 +63,25 @@ end
     get_blockhashbyheight(key::Integer; url::String="", testnet::Bool=false)
     -> String
 
-Returns the hash of the block in the current best blockchain based on its 
+Returns the hash of the block in the current best blockchain based on its
+height (how many blocks it is after the Genesis Block).
+"""
+function get_block(key::Integer; url::String="", testnet::Bool=false)
+    url *= init_url(url, testnet) *"/rest/blockhashbyheight/" * string(key) * ".bin"
+    response = HTTP.request("GET", url)
+    try
+        response.status == 200
+    catch
+        error("Unexpected status: ", response.status)
+    end
+    return bytes2hex(reverse(response.body))
+end
+
+"""
+    get_blockhashbyheight(key::Integer; url::String="", testnet::Bool=false)
+    -> String
+
+Returns the hash of the block in the current best blockchain based on its
 height (how many blocks it is after the Genesis Block).
 """
 function get_blockhashbyheight(key::Integer; url::String="", testnet::Bool=false)
